2d4beb5d2f73fc87f5b6bc2125ade616a686464e
adamd <adam.d.hinton@gmail.com>
2024-03-02 22:34:22 -0500

    enum notes

--- src/dev/lpa/Topping.java
+++ src/dev/lpa/Topping.java
@@ -1,4 +1,12 @@
 package dev.lpa;
 
 public enum Topping {
+    // These are really just a class, and the constants are instnaces of the class. We can add fields, frex.
+
+    MUSTARD,
+    PICKLES,
+    BACON,
+    BHEDDAR,
+    TOMATO
+
 }
